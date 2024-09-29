d73f441d89e200b43484949e6b117c3df7266f96
ageub <Borzhesh@yandex.ru>
2024-09-29 15:40:20 +0300

    Подправил тест на добавление задач в историю

--- src/tests/InMemoryHistoryManagerTest.java
+++ src/tests/InMemoryHistoryManagerTest.java
@@ -1,18 +1,16 @@
 package tests;
 
+
 import java.util.List;
 import manager.InMemoryHistoryManager;
 import org.junit.jupiter.api.Assertions;
-import task.Task;
-
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import task.Task;
 import task.TaskStatus;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class InMemoryHistoryManagerTest {
+public class InMemoryHistoryManagerTest {
     private InMemoryHistoryManager historyManager;
 
     @BeforeEach
@@ -21,17 +19,13 @@ class InMemoryHistoryManagerTest {
     }
 
     @Test
-    void testAddSameTask() {
+    public void testAddTask() {
         Task task1 = new Task("задача 1", "Выполнить задачу 1", TaskStatus.NEW);
 
-        historyManager.add(task1);
         historyManager.add(task1);
 
         List<Task> history = historyManager.getHistory();
 
-        assertEquals(1, history.size());
-        assertEquals(task1, history.get(0));
+        Assertions.assertEquals(task1, history.get(0));
     }
-
-
 }
